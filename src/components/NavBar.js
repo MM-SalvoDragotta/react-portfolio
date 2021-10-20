957aba898458a699b5f953f4b85d62191c377dcb
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-21 09:26:48 +1100

    Add sticky prop

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,10 @@ import React from 'react';
 import resume from '../assets/Resume - Salvo Dragotta.pdf';
 
 
-export default function Nav() {
+export default function Nav({ sticky }) {
     return (
      <div>
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <nav className= {sticky ? "navbar navbar-expand-lg navbar-light fixed-top navbar-sticky" : "navbar navbar-expand-lg navbar-light fixed-top"} id="mainNav">
             <div className="container px-4 px-lg-5">
                 <a className="navbar-brand" href="#page-top">Salvo Dragotta</a>
                 {/* <button className="navbar-toggler navbar-toggler-right" 
