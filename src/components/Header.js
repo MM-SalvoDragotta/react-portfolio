89794ebb0b16b7a9693fc383be15178b6054a8a9
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-21 09:25:54 +1100

    minor changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 export default function Head() {
     return (
         <div>    
-            <div className="masthead">
+            <div className="masthead" >
                 <div className="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
                     <div className="d-flex justify-content-center">
-                    <div className="text-center">
+                    <div className="text-center" >
                         <h1 className="mx-auto my-0 text-uppercase">Full Stack Development</h1>
                         <h2 className="text-white-50 mx-auto mt-2 mb-5">
                             <i className="fab fa-html5 fa-3x" style= {{color: '#FF7F50'}}></i> 
