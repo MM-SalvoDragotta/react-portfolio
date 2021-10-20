94163f03fabef82d4062b37babfe7027ce2167ed
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-21 09:24:03 +1100

    Add sticky hook navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 // We import useState and useEffect in our component
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import useSticky from "./hooks/useSticky.js"
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import About from './components/About';
 
 import './assets/css/jass.css';
 import './assets/css/style.css';
 
 function App() {
+  const { isSticky, element } = useSticky()
   return (
     <div>
     <Router>
-    <div className="App">
-      <NavBar style={{ marginBottom: 500}}/>             
-    </div>
-   
-
-    <Header /> 
+      
+      <div className="App">
+        <NavBar sticky={isSticky}/>             
+      </div>
+      <Header sticky={element} /> 
+      <About sticky={element}/>
+    
     </Router>    
     </div>
 
