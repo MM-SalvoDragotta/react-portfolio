d44021859c97462239e3cad94422b79da79d13c3
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-20 22:43:59 +1100

    Add Navbar Component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,16 @@ export default function Nav() {
         <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div className="container px-4 px-lg-5">
                 <a className="navbar-brand" href="#page-top">Salvo Dragotta</a>
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                {/* <button className="navbar-toggler navbar-toggler-right" 
+                type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarResponsive" 
+                aria-controls="navbarResponsive" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation">
                     Menu
                     <i className="fas fa-bars"></i>
-                </button>
-                <div className="collapse navbar-collapse w-75 dual-collapse2" id="navbarResponsive">
+                </button> */}
+                <div className="navbar-collapse w-75 dual-collapse2" id="navbarResponsive">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
                         <li className="nav-item"><a className="nav-link" href="#projects">Projects</a></li>
