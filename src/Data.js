1e8f69767fb0efaff20eaf2726ba390f90fe4fa7
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-22 09:35:08 +1100

    minor change

--- src/Data.js
+++ src/Data.js
@@ -10,9 +10,9 @@ export const projects = [
     "This project is for an online department store customer and admin side."
     "All items are listed on the website, with their quantities and prices."
     "The admin can change stock, prices, delete products, add products and show current promotions."
-    "The application runs a <strong className="bold-red">RESTful API </strong>to retrieve or create data from it's database."
+    "The application runs a <strong class="bold-red">RESTful API </strong>to retrieve or create data from it's database."
     "The application is displayed with both front-end and back-end models, with authentication needed for the back-end (admin) side."
-    "The application uses <strong className="bold-red">Node.js, Express.js, Handlebars.js, MySQL, Sequelize ORM, Heroku.</strong>"`,
+    "The application uses <strong class="bold-red">Node.js, Express.js, Handlebars.js, MySQL, Sequelize ORM, Heroku.</strong>"`,
     gif: groceriesGif,
     appUrl: "https://store-back-end-deployed.herokuapp.com/",
     repo :  "https://github.com/MM-SalvoDragotta/grocery-delivery-application" 
