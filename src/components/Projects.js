1c73f8fb986d44e518d325e9e213a88d909ab395
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-22 23:47:50 +1100

    Add AllProjects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Featured from './Featured';
+import AllProjects from './AllProjects';
 import { projects } from '../Data.js';
 
 export default function projectsSection() {
     const featuredProject = projects.filter((project) => {
-        return project.id === "Grocery Delivery App"
+        return project.id === 1
     })
-    console.log(featuredProject);
+    // console.log(featuredProject);
+    const restOfProjects = projects.slice(1);
+    // console.log(restOfProjects);
     return (        
         <section className="projects-section bg-light" id="projects">            
-            <Featured key={featuredProject[0].id} featuredProject={featuredProject[0]}/>               
+            <Featured key={featuredProject[0].id} featuredProject={featuredProject[0]}/>
+            {restOfProjects.map((project) => (
+              <AllProjects key={project.id} project={project} />
+            ))}                        
         </section>
     )
 }
