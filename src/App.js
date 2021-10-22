52adc328b274e7d0e312d1d6040da852aac9e922
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-23 00:18:29 +1100

    Add component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 import './assets/css/jass.css';
 import './assets/css/style.css';
@@ -19,6 +20,7 @@ function App() {
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </Router>    
     </div>
 
