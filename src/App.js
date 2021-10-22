d950d2ea71d4ff30040ff58dff6e6dc3fe3ab7c6
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-22 23:44:28 +1100

    Add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// We import useState and useEffect in our component
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 import './assets/css/jass.css';
 import './assets/css/style.css';
@@ -13,15 +13,13 @@ import './assets/css/style.css';
 function App() {  
   return (
     <div>
-    <Router>      
-      <div className="App">
-        <NavBar/>             
-      </div>
-      <Header /> 
-      <About />
-      <Projects />
-    
-    </Router>    
+      <Router>        
+        <NavBar/>           
+        <Header /> 
+        <About />
+        <Projects />
+        <Contact />
+      </Router>    
     </div>
 
   );
