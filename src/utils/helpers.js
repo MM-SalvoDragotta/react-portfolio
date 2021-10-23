7f6ffba1609086b7fcc5b157083b167dab9c9cd9
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-10-24 01:16:24 +1100

    Fix email validation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,5 @@
 export function validateEmail(email) {
-	var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 	return re.test(String(email).toLowerCase());
 }
 
-export function checkBlank(field) {
-	var re = /^\s*$/;
-	return re.test(String(field).toLowerCase());
-}
